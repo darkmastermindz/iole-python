125465c06567b47098615d7fd467862109adec6c
wariorx <rcalcamo@uncc.edu>
2017-10-29 00:27:07 -0400

    Started Console App

---
+++ tree_init.py
@@ -0,0 +1,10 @@
+
+# This file is meant to take in a raw string from a csv file and parse it into a matrix/hash
+
+
+def run(str_csv):
+   arr = str_csv.split("\n") #this returns an array where the first column is just labels
+
+   print(arr)
+
+
