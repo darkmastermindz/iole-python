9f80afb6bc54c2a0abca01e6c55df2ce7264c486
wariorx <rcalcamo@uncc.edu>
2017-10-29 09:01:41 -0400

    average set up almost working, need to fix the return none

--- hash_generator.py
+++ hash_generator.py
@@ -1,18 +1,25 @@
 
 # Gets raw string from a csv format and parses it into a hash
 import tree
+import regression
 
 
-def run(str_csv):
+def run(str_csv, func):
     arr = str_csv.split("\n") # this returns an array where the first column is just labels
     student_hash = {}
     index = 1 # skip first index
     while index < len(arr) - 1:
-        to_hash = arr[index].split("\t")
+        to_hash = arr[index].split(",")
         id = to_hash[0]
         student_hash[id] = "" # hash based on student ID
         to_hash.remove(id)
         student_hash[id] = to_hash
         index += 1
 
-    return tree.run(student_hash)
+    if func == '1':
+        return tree.run(student_hash)
+    elif func == '2':
+       return regression.run(student_hash) # will later return the result from linear regression
+    else:
+        print("Invalid argument, expected 1(Pairing) or 2(Expected Outcome)")
+        exit(1)
---
+++ regression.py
@@ -0,0 +1,31 @@
+
+# should return a hash with the average values on its first index, then individual scores on each other index
+
+
+def run(student_hash):
+    ids = student_hash.keys()
+    num_questions = calc_num_questions(student_hash, ids)
+    build = []
+    avg_hash = get_avg_hash(student_hash, 1, num_questions, []) #answers should be found on odd numbers
+    print(avg_hash)
+
+
+def calc_num_questions(student_hash, ids):
+    return len(student_hash[ids[0]]) / 2
+
+
+def get_avg_hash(student_hash, answer, num_questions, avg_hash):
+    if answer <= num_questions:
+        avg_hash.append(calc_avg(student_hash, answer))
+        answer += 2
+        get_avg_hash(student_hash, answer, num_questions, avg_hash)
+    else:
+        return avg_hash
+
+
+def calc_avg(student_hash, answer):
+    sum = 0
+    for arr in student_hash:
+        sum += float(student_hash[arr][answer])
+
+    return sum / len(student_hash)
